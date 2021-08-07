643d56f1a1f9000a86d357574347aafb88d0d819
Jose Ramirez <jarcodallo@gmail.com>
2021-08-06 21:39:21 -0300

    Fix auth module tests

--- src/auth/auth.resolver.spec.ts
+++ src/auth/auth.resolver.spec.ts
@@ -1,12 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing'
 import { AuthResolver } from './auth.resolver'
+import { AuthService } from './auth.service'
 
 describe('AuthResolver', () => {
   let resolver: AuthResolver
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthResolver],
+      providers: [AuthResolver, { provide: AuthService, useValue: {} }],
     }).compile()
 
     resolver = module.get<AuthResolver>(AuthResolver)
--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,4 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing'
+import { getLoggerToken } from 'nestjs-pino'
+import { getTerraToken } from 'nestjs-terra'
 import { AuthService } from './auth.service'
 
 describe('AuthService', () => {
@@ -6,7 +8,19 @@ describe('AuthService', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [
+        AuthService,
+        {
+          provide: getLoggerToken(AuthService.name),
+          useValue: {
+            error: jest.fn(),
+          },
+        },
+        {
+          provide: getTerraToken(),
+          useValue: {},
+        },
+      ],
     }).compile()
 
     service = module.get<AuthService>(AuthService)
