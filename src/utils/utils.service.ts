1e72b9155546dd7ddd5a7db0a9f2fddf0e02e172
jess <jesse@soob.co>
2022-03-08 01:35:21 +0900

    chore: remove debug log

--- src/utils/utils.service.ts
+++ src/utils/utils.service.ts
@@ -17,18 +17,16 @@ export class UtilsService {
     const queues = new Array(2048).fill(true).map(() => Promise.resolve())
     let distr_i = 0
 
+    // eslint-ignore
     const _get = lcdService.apiRequester.get.bind(lcdService.apiRequester)
     const _getRaw = lcdService.apiRequester.getRaw.bind(lcdService.apiRequester)
     lcdService.apiRequester.get = async (a, b) => {
       distr_i = distr_i + (1 % 2048)
-      console.log('next', distr_i)
-
       return queues[distr_i].then(() => _get(a, b))
     }
 
     lcdService.apiRequester.getRaw = async (a) => {
       distr_i = distr_i + (1 % 2048)
-      console.log('next', distr_i)
       return queues[distr_i].then(() => _getRaw(a))
     }
   }
