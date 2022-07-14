2290173abb15f968009b1bb3b7bf51a7287359b5
Jeff Woo <jeff@terra.money>
2022-07-14 19:37:25 +0900

    fix: AuthorizationType model not being created

--- src/common/models/authorization.model.ts
+++ src/common/models/authorization.model.ts
@@ -10,12 +10,20 @@ import {
 export class SendAuthorization {
   @Field(() => [Coin])
   spend_limit!: Coin[]
+
+  constructor(spend_limit: Coin[]) {
+    this.spend_limit = spend_limit
+  }
 }
 
 @ObjectType()
 export class GenericAuthorization {
   @Field()
   msg!: string
+
+  constructor(msg: string) {
+    this.msg = msg
+  }
 }
 
 @ObjectType()
@@ -37,27 +45,35 @@ export class StakeAuthorization {
 
   @Field()
   authorization_type!: number
+
+  constructor(
+    authorization_type: number,
+    max_tokens?: Coin,
+    allow_list?: StakeAuthorizationValidators,
+    deny_list?: StakeAuthorizationValidators,
+  ) {
+    this.authorization_type = authorization_type
+    this.max_tokens = max_tokens
+    this.allow_list = allow_list
+    this.deny_list = deny_list
+  }
 }
 
 export class Authorization {
   static fromTerra(authorization: TerraAuthorization): SendAuthorization | GenericAuthorization | StakeAuthorization {
     if (authorization instanceof TerraSendAuthorization) {
-      return {
-        spend_limit: Coin.fromTerraCoins(authorization.spend_limit),
-      }
+      return new SendAuthorization(Coin.fromTerraCoins(authorization.spend_limit))
     }
 
     if (authorization instanceof TerraGenericAuthorization) {
-      return {
-        msg: authorization.msg,
-      }
+      return new GenericAuthorization(authorization.msg)
     }
 
-    return {
-      max_tokens: authorization.max_tokens ? Coin.fromTerraCoin(authorization.max_tokens) : undefined,
-      allow_list: { addresses: authorization?.allow_list?.address ?? [] },
-      deny_list: { addresses: authorization?.deny_list?.address ?? [] },
-      authorization_type: authorization?.authorization_type,
-    }
+    return new StakeAuthorization(
+      authorization.authorization_type,
+      authorization.max_tokens ? Coin.fromTerraCoin(authorization.max_tokens) : undefined,
+      { addresses: authorization?.allow_list?.address ?? [] },
+      { addresses: authorization?.deny_list?.address ?? [] },
+    )
   }
 }
