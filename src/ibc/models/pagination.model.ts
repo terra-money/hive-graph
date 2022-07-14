fa94af67036b04b2946295f53f116f1d7542640b
Jeff Woo <jeff@terra.money>
2022-07-14 22:24:26 +0900

    fix: pagination model optional params

--- src/ibc/models/pagination.model.ts
+++ src/ibc/models/pagination.model.ts
@@ -4,20 +4,20 @@ import { OrderBy } from 'src/common/enums/index'
 @InputType()
 export class PaginationOptions {
   @Field(() => String, { nullable: true })
-  limit!: string
+  limit?: string
 
   @Field(() => String, { nullable: true })
-  offset!: string
+  offset?: string
 
   @Field(() => String, { nullable: true })
-  key!: string
+  key?: string
 
   @Field(() => String, { nullable: true })
-  count_total!: string
+  count_total?: string
 
   @Field(() => String, { nullable: true })
-  reverse!: string
+  reverse?: string
 
   @Field(() => OrderBy, { nullable: true })
-  order_by!: keyof typeof OrderBy
+  order_by?: keyof typeof OrderBy
 }
